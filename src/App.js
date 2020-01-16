1506dada0c056e69e5f7531070d87091565855ca
Mathieu Fedrigo <mathieu.fedrigo@uts.edu.au>
2020-01-16 18:48:50 +0100

    Add jsonToD3Data

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import React, { Component } from "react";
 import "./App.css";
 import Graph from "./Graph";
+import parseJson from "./helpers/jsonToD3Data";
+import basic from "./samples/basic-query";
+import minimal from "./samples/minimal-query";
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      data: {
-        nodes: [
-          { name: "fruit", id: 1 },
-          { name: "apple", id: 2 },
-          { name: "orange", id: 3 },
-          { name: "banana", id: 4 }
-        ],
-        links: [
-          { source: 1, target: 2 },
-          { source: 1, target: 3 }
-        ]
-      }
-    };
+    const data = parseJson(minimal);
+    this.state = { data };
+    // this.state = {
+    //   data: {
+    //     nodes: [
+    //       { name: "fruit", id: 1 },
+    //       { name: "apple", id: 2 },
+    //       { name: "orange", id: 3 },
+    //       { name: "banana", id: 4 }
+    //     ],
+    //     links: [
+    //       { source: 1, target: 2 },
+    //       { source: 1, target: 3 }
+    //     ]
+    //   }
+    // };
+
+    // console.log(this.state.data);
   }
 
   render() {
---
+++ src/helpers/jsonToD3Data.js
@@ -0,0 +1,24 @@
+const parseJson = jsonObject => {
+  const dependencies = jsonObject.data[0].dependencies;
+  const nodes = Object.keys(dependencies).map(key => ({
+    name: key,
+    id: parseInt(key)
+  }));
+  nodes.push({
+    name: "2",
+    id: 2
+  });
+  nodes.sort((a, b) => a.id - b.id);
+  const links = [];
+  Object.entries(dependencies).forEach(([key, values]) =>
+    values.forEach(value =>
+      links.push({ source: parseInt(key) + 1, target: value + 1 })
+    )
+  );
+
+  console.log(nodes);
+  console.log(links);
+  return { nodes, links };
+};
+
+export default parseJson;
