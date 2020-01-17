d94a465725e1f9e4a388bbd08a39ae7b49115add
Mathieu Fedrigo <mathieu.fedrigo@uts.edu.au>
2020-01-17 17:55:10 +0100

    Refactor

--- src/helpers/jsonToD3Data.js
+++ src/helpers/jsonToD3Data.js
@@ -1,5 +1,6 @@
-const parseJson = jsonObject => {
-  const dependencies = jsonObject.data[0].dependencies;
+import {dependencies} from "d3/dist/package";
+
+const getNodes = dependencies => {
   const nodes = Object.keys(dependencies).map(key => ({
     name: key,
     id: parseInt(key)
@@ -9,13 +10,23 @@ const parseJson = jsonObject => {
     id: 2
   });
   nodes.sort((a, b) => a.id - b.id);
+  return nodes;
+};
+
+const getLinks = dependencies => {
   const links = [];
   Object.entries(dependencies).forEach(([key, values]) =>
     values.forEach(value =>
       links.push({ source: parseInt(key) + 1, target: value + 1 })
     )
   );
+  return links;
+};
 
+const parseJson = jsonObject => {
+  const dependencies = jsonObject.data[0].dependencies;
+  const nodes = getNodes(dependencies);
+  const links = getLinks(dependencies);
   return { nodes, links };
 };
 
