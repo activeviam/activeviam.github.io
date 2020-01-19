7233f33b2ddfe5f2a1c257b01c3c24aee1b9e228
Mathieu Fedrigo <mathieu.fedrigo@uts.edu.au>
2020-01-19 17:46:38 +0100

    Fix crash when clicking the '-1' node.

--- src/Graph/NodeDetail.js
+++ src/Graph/NodeDetail.js
@@ -2,18 +2,12 @@ import React, { Component } from "react";
 
 class NodeDetail extends Component {
   render() {
-    const {
-      type,
-      retrId,
-      measureProvider,
-      measures,
-      partitioning
-    } = this.props.details;
+    const { details } = this.props;
+    if (!details) return null;
+    const { type, retrId, measureProvider, measures, partitioning } = details;
     return (
       <div>
-        <b>
-          {`${type} (#${retrId})`}
-        </b>
+        <b>{`${type} (#${retrId})`}</b>
         <ul>
           <li>Measures provider: {measureProvider}</li>
           <li>
