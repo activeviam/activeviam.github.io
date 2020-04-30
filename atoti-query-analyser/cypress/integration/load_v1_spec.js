a75a8d37884de49058abb97bb13904e9dba885d7
Olivier Peyrusse <ope@activeviam.com>
2020-04-30 09:04:50 +0200

    Add more assertions to the sample working.

--- atoti-query-analyser/cypress/integration/load_v1_spec.js
+++ atoti-query-analyser/cypress/integration/load_v1_spec.js
@@ -1,37 +1,43 @@
 // / <reference types="cypress" />
 
 describe("Works with samples", () => {
-  // ["v1-retail.txt"].forEach(file => {
+  describe("support v1 inputs", () => {
+    ["v1-retail.txt"].forEach(file => {
+      it(`process ${file}`, () => {
+        cy.visit("http://localhost:3000");
+
+        cy.fixture(`exports/${file}`).then(input => {
+          cy.get("#query-input")
+            .invoke("val", input)
+            .trigger("change");
+        });
+
+        cy.contains("Import from V1").click();
+        cy.contains("MDX pass Select");
+
+        cy.contains("Graph").click();
+        cy.contains("Menu");
+
+        cy.contains("Timeline").click();
+      });
+    });
+  });
+
+  // ["larger-distributed-query.json"].forEach(file => {
   //   it(`process ${file}`, () => {
   //     cy.visit("http://localhost:3000");
 
   //     cy.fixture(`exports/${file}`).then(input => {
   //       cy.get("#query-input")
-  //         .invoke("val", input)
-  //         .trigger("change");
+  //         .invoke("val", JSON.stringify(input))
+  //         .trigger("change", { force: true });
   //     });
 
-  //     cy.contains("Import from V1").click();
+  //     // cy.get("#query-input").trigger("change");
+
+  //     cy.contains("Import from Json").click();
 
   //     cy.contains("MDX pass Select");
   //   });
   // });
-
-  ["larger-distributed-query.json"].forEach(file => {
-    it(`process ${file}`, () => {
-      cy.visit("http://localhost:3000");
-
-      cy.fixture(`exports/${file}`).then(input => {
-        cy.get("#query-input")
-          .invoke("val", JSON.stringify(input))
-          .trigger("change", { force: true });
-      });
-
-      // cy.get("#query-input").trigger("change");
-
-      cy.contains("Import from Json").click();
-
-      cy.contains("MDX pass Select");
-    });
-  });
 });
