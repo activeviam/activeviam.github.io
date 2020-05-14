5e7ed0ee68c871fdc1d20e493928778c91e09ded
ci-build <ci-build@activeviam.com>
2020-05-14 20:21:55 +0000

    Updates

--- atoti-query-analyser/precache-manifest.44da2291356bb96f83ce17281b50bacc.js
+++ atoti-query-analyser/precache-manifest.44da2291356bb96f83ce17281b50bacc.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "4e50370913687ec09752c9911637cb9d",
+    "revision": "c2656d1da9952ef17e6ef44955da35cd",
     "url": "./index.html"
   },
   {
-    "revision": "38188752d2e00b4bcaf9",
+    "revision": "2fa95c0d1d42c130ba6b",
     "url": "./static/css/2.69121389.chunk.css"
   },
   {
-    "revision": "0ef53f13adfd7d9a59ce",
+    "revision": "037bf5730bfe65019032",
     "url": "./static/css/main.49ac033c.chunk.css"
   },
   {
-    "revision": "38188752d2e00b4bcaf9",
-    "url": "./static/js/2.ac8a12f9.chunk.js"
+    "revision": "2fa95c0d1d42c130ba6b",
+    "url": "./static/js/2.1aa9040b.chunk.js"
   },
   {
     "revision": "c836aaadfe43dfd9fcb8ddeddfd61e18",
-    "url": "./static/js/2.ac8a12f9.chunk.js.LICENSE.txt"
+    "url": "./static/js/2.1aa9040b.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "0ef53f13adfd7d9a59ce",
-    "url": "./static/js/main.4087b307.chunk.js"
+    "revision": "037bf5730bfe65019032",
+    "url": "./static/js/main.7d453a03.chunk.js"
   },
   {
     "revision": "dfa479a94cbfe8aba3e7",
--- atoti-query-analyser/service-worker.js
+++ atoti-query-analyser/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.bfbb2eac19f2b7727970c68e1fe7c6e0.js"
+  "./precache-manifest.44da2291356bb96f83ce17281b50bacc.js"
 );
 
 self.addEventListener('message', (event) => {
