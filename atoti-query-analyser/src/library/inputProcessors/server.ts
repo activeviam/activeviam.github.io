f591e6c3651dbf64830064738db3a53e609871c3
Olivier Peyrusse <kineolyan@proton.me>
2024-03-20 10:12:53 +0100

    Fix query plan retrieval from a running server. (#75)
    Resolves #71

--- atoti-query-analyser/src/library/inputProcessors/server.ts
+++ atoti-query-analyser/src/library/inputProcessors/server.ts
@@ -18,6 +18,21 @@ function cleanseUrl(url: string) {
   return match[1];
 }
 
+const supportedApis = ["5", "6", "8"];
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const findServiceUrl = (apis: any) => {
+  const pivotApi = apis["activeviam/pivot"] ?? apis.pivot;
+  if (pivotApi !== undefined) {
+    for (const version of pivotApi.versions) {
+      if (supportedApis.includes(version.id)) {
+        return version.restPath;
+      }
+    }
+  }
+  throw new Error("Not supporting this server version.");
+};
+
 /**
  * Resolves the version of the API of the target server.
  * @param {String} url url of the target server
@@ -34,8 +49,13 @@ function resolveQueryEndpoint(userUrl: string) {
     fetch(`${url}/versions/rest`).then(
       async (response) => {
         const versions = await response.json();
-        const pivotService = versions.apis.pivot.versions[0].restPath;
-        resolve(`${url}${pivotService}`);
+        const serviceUrl = findServiceUrl(versions.apis);
+        const fullUrl =
+          `${url}/${serviceUrl}/cube/query/mdx/queryplan`.replaceAll(
+            /\/{2,}/g,
+            "/"
+          );
+        resolve(fullUrl);
       },
       (err) => {
         console.error(`Cannot find version of ${url}`, err);
@@ -50,8 +70,9 @@ function resolveQueryEndpoint(userUrl: string) {
 
 export interface ServerInput {
   url: string;
-  credentials: string;
-  query: string;
+  username: string;
+  password: string;
+  mdxQuery: string;
 }
 
 /**
@@ -59,12 +80,17 @@ export interface ServerInput {
  * @param payload payload to send to the server
  * @returns the exported query plan for the provided query
  */
-export async function queryServer({ url, credentials, query }: ServerInput) {
-  const baseUrl = await resolveQueryEndpoint(url);
-  const queryUrl = `${baseUrl}/cube/query/mdx/queryplan`;
+export async function queryServer({
+  url,
+  mdxQuery,
+  username,
+  password,
+}: ServerInput) {
+  const queryUrl = await resolveQueryEndpoint(url);
   const body = {
-    mdx: query,
+    mdx: mdxQuery,
   };
+  const credentials = btoa(`${username}:${password}`);
   const response = await fetch(queryUrl, {
     method: "POST",
     mode: "cors",
@@ -77,5 +103,6 @@ export async function queryServer({ url, credentials, query }: ServerInput) {
     body: JSON.stringify(body),
   });
   const payload = await response.json();
-  return payload.data;
+  // Old services wrap in {success, data}; new services don't
+  return payload.data ?? payload;
 }
