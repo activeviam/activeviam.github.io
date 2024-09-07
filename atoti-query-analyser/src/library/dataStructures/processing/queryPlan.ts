63139f4892a3b0182beb16f6a08c804f9865b71c
Kineolyan <kineolyan@protonmail.com>
2024-09-07 21:28:55 +0200

    Process results to fill missing result sizes

--- atoti-query-analyser/src/library/dataStructures/processing/queryPlan.ts
+++ atoti-query-analyser/src/library/dataStructures/processing/queryPlan.ts
@@ -13,6 +13,35 @@ export interface QueryPlan {
   querySummary: QuerySummary;
 }
 
+const preprocessResultSize = (graph: RetrievalGraph) => {
+  for (const node of graph.getVertices()) {
+    const retrieval = node.getMetadata();
+    const resultSizes = Reflect.get(retrieval, "resultSizes");
+    const elapsedTimes = retrieval.timingInfo.elapsedTime;
+    if (
+      resultSizes &&
+      Array.isArray(resultSizes) &&
+      elapsedTimes !== undefined
+    ) {
+      if (resultSizes.length < elapsedTimes.length) {
+        // Try to fill the missing result sizes by setting a size of 0 for operations of duration 0.
+        // They are likely to not have return any result, causing this very short duration
+        // we will only do this if the count of operations of duration 0 matches the count of missing sizes
+        const zeroOperationCount = elapsedTimes.filter(
+          (time) => time === 0
+        ).length;
+        if (zeroOperationCount === elapsedTimes.length - resultSizes.length) {
+          let sizePosition = 0;
+          const processedResultSizes = elapsedTimes.map((time) =>
+            time === 0 ? 0 : resultSizes[sizePosition++]
+          );
+          Reflect.set(retrieval, "resultSizes", processedResultSizes);
+        }
+      }
+    }
+  }
+};
+
 /**
  * Given an output of `JSON.parse()`, convert it into
  * {@link "library/dataStructures/json/jsonQueryPlan"!JsonQueryPlan}, and then
@@ -45,6 +74,7 @@ export function preprocessQueryPlan(json: unknown): QueryPlan[] {
         if (needFillTimingInfo) {
           setSimulatedTimingInfo(graph);
         }
+        preprocessResultSize(graph);
 
         return {
           planInfo,
